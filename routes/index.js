3376bbe6845d76838d496edf49a52b42fbb05667
chineduCoded <chinedujohn17@gmail.com>
2023-09-04 21:44:33 +0000

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ const router = express.Router();
 
 import AppController from "../controllers/AppController";
 import UsersController from "../controllers/UsersController";
+import AuthController from "../controllers/AuthController";
 
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
 router.post("/users", UsersController.postNew);
+router.get("/connect", AuthController.getConnect);
+router.get("/disconnect", AuthController.getDisconnect);
+router.get("/users/me", UsersController.getMe);
 
-export default router;
+module.exports = router;
