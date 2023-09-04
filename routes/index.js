5b9425de22e47a4991dddb12714bd23378eee8b8
chineduCoded <chinedujohn17@gmail.com>
2023-09-04 22:21:08 +0000

    First file

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 import AppController from "../controllers/AppController";
 import UsersController from "../controllers/UsersController";
 import AuthController from "../controllers/AuthController";
+import FilesController from "../controllers/FilesController";
 
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
@@ -11,5 +12,6 @@ router.post("/users", UsersController.postNew);
 router.get("/connect", AuthController.getConnect);
 router.get("/disconnect", AuthController.getDisconnect);
 router.get("/users/me", UsersController.getMe);
+router.post("/files", FilesController.postUpload);
 
 module.exports = router;
