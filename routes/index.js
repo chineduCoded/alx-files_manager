ad4769c7cff60aa57696969bfcac593f570c5994
chineduCoded <chinedujohn17@gmail.com>
2023-09-05 00:47:13 +0000

    File publish/unpublish

--- routes/index.js
+++ routes/index.js
@@ -25,4 +25,8 @@ router.post("/files", FilesController.postUpload);
 router.get("/files/:id", FilesController.getShow);
 router.get("/files", FilesController.getIndex);
 
+// File publish/unpublish
+router.put("/files/:id/publish", FilesController.putPublish);
+router.put("/files/:id/unpublish", FilesController.putUnpublish);
+
 module.exports = router;
